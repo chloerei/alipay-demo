1cf1973d2a4343cc5764f88c4f8ac5b9237888a8
Rei <chloerei@gmail.com>
2018-03-03 17:47:47 +0800

    Add home index

--- config/application.rb
+++ config/application.rb
@@ -34,5 +34,10 @@ module App
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+
+    config.generators do |generate|
+      generate.helper false
+      generate.assets false
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  root to: 'home#index'
+
   resource :alipay_trade_page_pay do
     get :done
     post :notify
